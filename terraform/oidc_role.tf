9200c9dc7248c8a343d1ddda6196835f3ebc643e
Max Neuvians <max@neuvians.net>
2022-09-22 14:03:28 +0000

    feat/terraform_demo

---
+++ terraform/oidc_role.tf
@@ -0,0 +1,51 @@
+locals {
+  plan_name  = "AWSLambdaTrafficShiftingDemoReadOnlyRole"
+  admin_name = "AWSLambdaTrafficShiftingDemoAdministratorRole"
+}
+
+data "aws_caller_identity" "current" {}
+
+module "gh_oidc_roles" {
+  source = "github.com/cds-snc/terraform-modules?ref=v3.0.17//gh_oidc_role"
+  roles = [
+    {
+      name      = local.plan_name
+      repo_name = "aws-lambda-traffic-shifting-action"
+      claim     = "*"
+    },
+    {
+      name      = local.admin_name
+      repo_name = "aws-lambda-traffic-shifting-action"
+      claim     = "ref:refs/heads/main"
+    }
+  ]
+
+  billing_tag_value = var.billing_code
+}
+
+module "attach_tf_plan_policy" {
+  source            = "github.com/cds-snc/terraform-modules?ref=v3.0.17//attach_tf_plan_policy"
+  account_id        = data.aws_caller_identity.current.account_id
+  role_name         = local.plan_name
+  bucket_name       = "${var.billing_code}-tf"
+  lock_table_name   = "terraform-state-lock-dynamo"
+  billing_tag_value = var.billing_code
+  depends_on = [
+    module.gh_oidc_roles
+  ]
+}
+
+data "aws_iam_policy" "admin" {
+  name = "AdministratorAccess"
+  depends_on = [
+    module.gh_oidc_roles
+  ]
+}
+
+resource "aws_iam_role_policy_attachment" "admin" {
+  role       = local.admin_name
+  policy_arn = data.aws_iam_policy.admin.arn
+  depends_on = [
+    module.gh_oidc_roles
+  ]
+}
