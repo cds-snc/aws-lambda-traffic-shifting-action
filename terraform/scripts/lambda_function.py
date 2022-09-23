7993c030732447edf181f2985c63c268a1483b7d
Max Neuvians <max@neuvians.net>
2022-09-23 19:39:31 +0000

    feat: fix lambda

--- terraform/scripts/lambda_function.py
+++ terraform/scripts/lambda_function.py
@@ -1,4 +1,9 @@
 import json
 
 def lambda_handler(event, context):
-    print(1/0)
+    # print(1/0) This was the offending code
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
